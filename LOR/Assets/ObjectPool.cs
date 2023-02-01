34227fc406a3b03b48afbbfb33208a9a0dcdfe35
BlackPenguin-cpu <dkstmdwnswns@gmail.com>
2023-02-01 19:51:12 +0900

    ObjectPool Complete

--- LOR/Assets/ObjectPool.cs
+++ LOR/Assets/ObjectPool.cs
@@ -1,21 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class ObjectPoolClass
 {
-    public GameObject parentObj;
+    public GameObject parentObj, activeGroupObj, inactiveGroupObj;
     public Queue<GameObject> objNotActiveQueue = new Queue<GameObject>();
-    public Queue<GameObject> objActiveQueue = new Queue<GameObject>();
+    /// <summary>
+    /// 해당 풀이 가질수 있는 활성화된 오브젝트의 최대 갯수. 값이 -1일경우 제한 없음
+    /// </summary>
+    public int maxObjCount = -1;
 
-    public ObjectPoolClass(GameObject _parentObj)
+    public ObjectPoolClass(GameObject _parentObj, GameObject _activeGroupObj, GameObject _inactiveGroupObj, int _maxObjCount = -1)
     {
         parentObj = _parentObj;
-        objActiveQueue = new Queue<GameObject>();
+        activeGroupObj = _activeGroupObj;
+        inactiveGroupObj = _inactiveGroupObj;
         objNotActiveQueue = new Queue<GameObject>();
+        maxObjCount = _maxObjCount;
     }
 }
 public class ObjectPool : MonoBehaviour
@@ -27,6 +33,11 @@ public class ObjectPool : MonoBehaviour
 
         if (parentObjList.ContainsKey(obj.name))
         {
+            if (parentObjList[obj.name].maxObjCount != -1 && parentObjList[obj.name].maxObjCount < parentObjList[obj.name].activeGroupObj.transform.childCount)
+            {
+                returnObj(parentObjList[obj.name].activeGroupObj.transform.GetChild(0).gameObject);
+                return createObj(obj);
+            }
             //미리 생성되어 비활성화된 오브젝트풀 오브젝트를 체크합니다
             if (parentObjList[obj.name].objNotActiveQueue.Count > 0)
             {
@@ -39,16 +50,21 @@ public class ObjectPool : MonoBehaviour
             else
             {
                 var poolObj = Instantiate(obj);
-                parentObjList[obj.name].objNotActiveQueue.Enqueue(poolObj);
+                poolObj.transform.parent = parentObjList[obj.name].activeGroupObj.transform;
                 return poolObj;
             }
         }
         else
         {
             var parentObj = new GameObject($"{obj.name}_Parent");
-            var objectPoolClass = new ObjectPoolClass(parentObj);
+            var activeObjPool = new GameObject($"{obj.name}__ActiveObjs");
+            var inactiveObjPool = new GameObject($"{obj.name}_InactiveObjs");
+            var objectPoolClass = new ObjectPoolClass(parentObj, activeObjPool, inactiveObjPool);
 
-            parentObj.transform.parent = this.gameObject.transform;
+            activeObjPool.transform.parent = parentObj.transform;
+            inactiveObjPool.transform.parent = parentObj.transform;
+
+            activeObjPool.transform.parent = this.gameObject.transform;
 
             parentObjList.Add(obj.name, objectPoolClass);
             return createObj(obj);
@@ -61,5 +77,34 @@ public class ObjectPool : MonoBehaviour
         localObj.transform.position = pos;
         localObj.transform.rotation = quaternion;
         return localObj;
+    }
+
+    public void returnObj(GameObject obj)
+    {
+        if (parentObjList.ContainsKey(textCloneRemove(obj.name)))
+        {
+            string objOriginalName = textCloneRemove(obj.name);
+
+            obj.SetActive(false);
+            obj.transform.parent = parentObjList[objOriginalName].inactiveGroupObj.transform;
+            parentObjList[objOriginalName].objNotActiveQueue.Enqueue(obj);
+        }
+        else
+        {
+            Debug.Log("경고. 오브젝트풀 오브젝트가 아닌 오브젝트를 오브젝트풀 형식으로 해제하려 하였습니다.");
+            Destroy(obj);
+        }
+    }
+
+    public void MaxObjCountChange(GameObject targetObj, int objCount)
+    {
+        parentObjList[textCloneRemove(targetObj.name)].maxObjCount = objCount;
+    }
+
+    private string textCloneRemove(string objName)
+    {
+        StringBuilder builder = new StringBuilder(objName);
+        builder.Replace("(Clone)", "");
+        return builder.ToString();
     }
 }
