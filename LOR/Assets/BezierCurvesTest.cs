d9cd46ba7f14113d8b6227c94b82c97225f6ec3a
BlackPenguin-cpu <dkstmdwnswns@gmail.com>
2023-03-06 20:54:23 +0900

    BezierCurves

--- LOR/Assets/BezierCurvesTest.cs
+++ LOR/Assets/BezierCurvesTest.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class BezierCurvesTest : MonoBehaviour
 {
+    public GameObject Target;
     public Transform[] pos;
+    [Range(0f, 1f)]
     public float curveValue;
     private void Start()
     {
@@ -14,9 +17,25 @@ public class BezierCurvesTest : MonoBehaviour
     private IEnumerator CurveCoroutine()
     {
         Vector3 point1;
+        Vector3 point2;
+        Vector3 point3;
+
+
+        Vector3 point4;
+        Vector3 point5;
+
+        Vector3 point6;
         for (int i = 0; i < pos.Length; i++)
         {
-            Vector3.Lerp(pos[0], pos[1], i);
+            point1 = Vector3.Lerp(pos[0].position, pos[1].position, curveValue);
+            point2 = Vector3.Lerp(pos[1].position, pos[2].position, curveValue);
+            point3 = Vector3.Lerp(pos[2].position, pos[3].position, curveValue);
+
+            point4 = Vector3.Lerp(point1, point2, i);
+            point5 = Vector3.Lerp(point2, point3, i);
+
+            point6 = Vector3.Lerp(point4, point5, i);
+            Target.transform.position = point6;
         }
         yield return null;
 
