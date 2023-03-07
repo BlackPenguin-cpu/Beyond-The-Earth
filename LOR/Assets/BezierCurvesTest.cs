ef262fb380ad76610e9515292bacaa357de202e6
BlackPenguin-cpu <dkstmdwnswns@gmail.com>
2023-03-07 17:26:54 +0900

    BazierCurve Auto

--- LOR/Assets/BezierCurvesTest.cs
+++ LOR/Assets/BezierCurvesTest.cs
@@ -16,28 +16,34 @@ public class BezierCurvesTest : MonoBehaviour
     //3차 베지어 곡선 그래프
     private IEnumerator CurveCoroutine()
     {
+        for (float i = 0; i < 1; i += Time.deltaTime)
+        {
+            Target.transform.position = BezierCurve(pos, i);
+            yield return null;
+        }
+
+    }
+
+    private Vector3 BezierCurve(Transform[] pos, float i)
+    {
         Vector3 point1;
         Vector3 point2;
         Vector3 point3;
-
-
+        
         Vector3 point4;
         Vector3 point5;
 
         Vector3 point6;
-        for (int i = 0; i < pos.Length; i++)
-        {
-            point1 = Vector3.Lerp(pos[0].position, pos[1].position, curveValue);
-            point2 = Vector3.Lerp(pos[1].position, pos[2].position, curveValue);
-            point3 = Vector3.Lerp(pos[2].position, pos[3].position, curveValue);
 
-            point4 = Vector3.Lerp(point1, point2, i);
-            point5 = Vector3.Lerp(point2, point3, i);
+        point1 = Vector3.Lerp(pos[0].position, pos[1].position, curveValue);
+        point2 = Vector3.Lerp(pos[1].position, pos[2].position, curveValue);
+        point3 = Vector3.Lerp(pos[2].position, pos[3].position, curveValue);
 
-            point6 = Vector3.Lerp(point4, point5, i);
-            Target.transform.position = point6;
-        }
-        yield return null;
+        point4 = Vector3.Lerp(point1, point2, i);
+        point5 = Vector3.Lerp(point2, point3, i);
 
+        point6 = Vector3.Lerp(point4, point5, i);
+
+        return point6;
     }
 }
