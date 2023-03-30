d547de7f2a761eb38cbdf3e667a4ed4ff7584c5f
Black_Penguin <dkstmdwnswns@gmail.com>
2023-03-30 15:49:20 +0900

    Shotgun Enemy

--- LOR/Assets/Scripts/Enemy/ShotGunEnemy.cs
+++ LOR/Assets/Scripts/Enemy/ShotGunEnemy.cs
@@ -6,6 +6,13 @@ public class ShotGunEnemy : BaseEnemy
 {
     public float attackDelay;
     private float nowAttackDelay;
+
+    private NormalBullet normalBullet;
+
+    private void Start()
+    {
+        normalBullet = Resources.Load<NormalBullet>("Bullet/NormalBullet");
+    }
     protected override void Update()
     {
         base.Update();
@@ -13,12 +20,27 @@ public class ShotGunEnemy : BaseEnemy
         if (nowAttackDelay < 0)
         {
             nowAttackDelay = attackDelay;
+            Attack();
         }
+    }
+    private void Attack()
+    {
+        if (transform.position.z < Player.instance.transform.position.z) return;
+        for (int i = 0; i < 10; i++)
+        {
 
+            NormalBullet bullet = Instantiate(normalBullet, transform.position, Quaternion.Euler(Random.Range(-10, 10), Random.Range(-10, 10) + 180, transform.rotation.z));
+
+            bullet.speed = 30;
+            bullet.attackDmg = attackDmg;
+            bullet.lifeTime = 3;
+            bullet.isPlayerBullet = false;
+        }
     }
     protected override void Move()
     {
         base.Move();
-        transform.position += transform.forward * Time.deltaTime * speed;
+        transform.position += Vector3.back * Time.deltaTime * speed +
+            new Vector3(Mathf.Cos(Time.time), Mathf.Sin(Time.time)) * 5 * Time.deltaTime;
     }
 }
