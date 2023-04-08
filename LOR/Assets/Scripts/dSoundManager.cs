fdb3dbac86d44c73ec88efb2bc1372dcb85a4b3f
Black_Penguin <dkstmdwnswns@gmail.com>
2023-04-08 18:27:21 +0900

    go away anna

---
+++ LOR/Assets/Scripts/dSoundManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private Dictionary<string, AudioClip> audioClips;
+    public void PlaySound()
+    {
+
+    }
+
+}
